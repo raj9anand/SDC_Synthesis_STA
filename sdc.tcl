a56bd2e9658867bc8def66e746ce8fcbaf00db45
Anand Raj <60174790+raj9anand@users.noreply.github.com>
2025-12-29 10:55:53 +0530

    Update timing constraints in sdc.tcl

--- sdc.tcl
+++ sdc.tcl
@@ -1,5 +1,7 @@
-create_clock, set_input_delay, set_output_delay
+create_clock -name clk -period 10 [get_ports clk]
+set_input_delay 2 -clock clk [get_ports data_in]
+set_output_delay 3 -clock clk [get_ports data_out]
+set_clock_uncertainty 0.2 clk
+set_false_path -from [get_clocks clk1] -to [get_clocks clk2]
+set_multicycle_path 2 -from reg1 -to reg2
 
-set_false_path, set_multicycle_path
-
-set_clock_uncertainty, set_clock_latency
